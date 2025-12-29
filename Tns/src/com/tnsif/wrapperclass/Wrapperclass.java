96aa823f81c541aa80dddc922c64f30dd850518a
bhara <bhara@Lisa>
2025-12-29 11:14:51 +0530

    wrapper class

---
+++ Tns/src/com/tnsif/wrapperclass/Wrapperclass.java
@@ -0,0 +1,11 @@
+package com.tnsif.wrapperclass;
+
+public class Wrapperclass {
+	public static void main(String[] args) {
+		int x=5;
+		Integer y=Integer.valueOf(x);
+		int z=y;
+		System.out.println(y +" "+ z);
+
+	}
+}
